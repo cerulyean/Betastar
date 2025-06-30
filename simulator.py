a7f448efeef0c41eafeac25e68a1c980851f922a
cerulyean <e1122543@u.nus.edu>
2025-06-30 16:48:52 +0800

    Added data.json and updated simulator to use it instead

--- simulator.py
+++ simulator.py
@@ -556,7 +556,7 @@ def extract_data(replay_name: str, output_name: str, fow_pov, step_size: int = 2
 
 def process_folder(input_folder="1000 replays", output_folder="1000 extracts"):
     #extract only zvp games
-    with open("D:/betastar/parser/data.json", "r") as f:
+    with open("data.json", "r") as f:
         detailed_info = json.load(f)
     t0 = time.time()
     count = 0
